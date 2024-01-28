6671d44b8c663bc1abf87b8b1d374858ece5fdde
Strimmerz <strimme98@gmail.com>
2024-01-28 15:03:14 +0530

    flask add

--- app.py
+++ app.py
@@ -1,13 +1,55 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+import sys
+import os 
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 from src.mlproject.components.model_tranier import ModelTrainer,ModelTrainerConfig
 
+from sklearn.preprocessing import StandardScaler
+from src.mlproject.pipelines.prediction_pipelie import PredictPipeline,CustomData
 
-import sys
+application=Flask(__name__)
 
-if __name__ == "__main__":
+app = application
+
+##Route for a home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=="GET":
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+        
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+    app.run(debug=True)
     logging.info("The execution has started")
     
     try:
@@ -27,4 +69,6 @@ if __name__ == "__main__":
         
         logging.info("Custom Exception")
         raise CustomException(e,sys)
+    
+    
     
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
-
+#import dill
 import pickle
 import numpy as np
 
@@ -78,6 +78,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
